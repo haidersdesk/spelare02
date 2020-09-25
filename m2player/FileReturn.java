21ba8c0f9f6da2393704069f0a256b87a3a061bf
haidersdesk <67119669+haidersdesk@users.noreply.github.com>
2020-09-25 12:34:51 +0200

    Add files via upload

---
+++ m2player/FileReturn.java
@@ -0,0 +1,38 @@
+package m2player;
+
+
+import java.io.File;
+import javax.swing.filechooser.FileFilter;
+
+
+public class FileReturn extends FileFilter{
+    
+    // File Extentions String
+    private final String extension;
+    // File Extension Description
+    private final String description;
+    
+    // Constructor Method
+    public FileReturn(String extension, String description){
+        // Set Contructor Values
+        this.extension = extension;
+        this.description = description;
+    }
+    
+    @Override
+    public boolean accept(File file) {
+        // If File Is Returning Directory
+        if(file.isDirectory()){
+            return true;
+        }
+        // Return File Name with Extension
+        return file.getName().endsWith(extension);
+    }
+
+    @Override
+    public String getDescription() {
+        // Return To Display File Type and Description
+        return description + String.format(" (*%s)", extension);
+    }
+    
+}
